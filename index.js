1df612513906ce7c0afddb02eaed2a0c5ed2b533
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-09 20:12:14 -0400

    commit tree 7d4a7ac96e6fa10fc9d9a1bec6090cdac2d785a0

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 /* Your Code Here */
+function createEmployeeRecord(arr){
+    let employeeRecord = {   
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [] ,
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
 
+let createEmployeeRecords =  function(record){
+    return record.map(createEmployeeRecord)
+}
+
+let createTimeInEvent = function(dateStamp){
+    const newTimeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeInEvents.push(newTimeIn)
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    const newTimeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeOutEvents.push(newTimeOut)
+    return this
+}
+
+let hoursWorkedOnDate = function(date){
+    const timeIn = this.timeInEvents.find(e => e.date === date).hour
+    const timeOut  = this.timeOutEvents.find( e => e.date === date).hour
+    return (timeOut - timeIn)/100;
+}
+
+let wagesEarnedOnDate = function(date){
+    const pay = this.payPerHour
+    return pay * hoursWorkedOnDate.call(this, date)
+}
+
+function findEmployeeByFirstName(empNames, firstName){
+    return empNames.find(employee => employee.firstName == firstName)
+}
+
+let calculatePayroll = function(rec){
+    return rec.reduce(function(total, wages){
+         return total + allWagesFor.call(wages)}, 0)
+ }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
